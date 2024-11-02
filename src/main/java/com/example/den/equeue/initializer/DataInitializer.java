ec7f03d078973f736390bedf14a71156592a96f3
Denys <denys.shl@mail.com>
2024-11-02 14:32:59 +0200

    Added docker-compose.yml with Redis

--- src/main/java/com/example/den/equeue/initializer/DataInitializer.java
+++ src/main/java/com/example/den/equeue/initializer/DataInitializer.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
@@ -15,6 +16,7 @@ import java.time.LocalTime;
 @RequiredArgsConstructor
 public class DataInitializer {
 
+    private final StringRedisTemplate stringRedisTemplate;
     private final BookingSlotRepository bookingSlotRepository;
 
     @EventListener(ApplicationReadyEvent.class)
@@ -24,15 +26,27 @@ public class DataInitializer {
     }
 
     private void initializeBookingSlots() {
-        log.info("Initializing booking slots...");
-        final LocalTime startTime = LocalTime.of(9, 0);
-        final LocalTime endTime = LocalTime.of(18, 0);
+        if (isInitialized()) {
+            log.info("Booking slots already initialized");
+        } else {
+            log.info("Initializing booking slots...");
+            final LocalTime startTime = LocalTime.of(9, 0);
+            final LocalTime endTime = LocalTime.of(18, 0);
 
-        for (LocalTime slot = startTime; slot.isBefore(endTime); slot = slot.plusMinutes(30)) {
-            final BookingSlot bookingSlot = new BookingSlot(slot.toString(), slot, slot.plusMinutes(30));
-            log.info("Slot: {}", slot);
+            for (LocalTime slot = startTime; slot.isBefore(endTime); slot = slot.plusMinutes(30)) {
+                final BookingSlot bookingSlot = new BookingSlot(slot.toString(), slot, slot.plusMinutes(30));
+                log.info("Slot: {} - bookingSlot: {}", slot);
 
-            bookingSlotRepository.save(bookingSlot);
+                bookingSlotRepository.save(bookingSlot);
+            }
+
+            stringRedisTemplate.opsForValue()
+                    .set("bookingSlots.initialized", "true");
+            log.info("Booking slots initialized");
         }
+    }
+
+    private boolean isInitialized() {
+        return Boolean.TRUE.equals(stringRedisTemplate.hasKey("bookingSlots.initialized"));
     }
 }
